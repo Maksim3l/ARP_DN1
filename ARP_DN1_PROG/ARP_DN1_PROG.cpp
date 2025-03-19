cbead79979944e382e9e721b52c3d414a1470541
Maksim3l <loknarmaksim@gmail.com>
2025-03-19 14:55:12 +0100

    Prepared libraries
    Prepared the necessary libraries that will be used and set the namespace to std.

--- ARP_DN1_PROG/ARP_DN1_PROG.cpp
+++ ARP_DN1_PROG/ARP_DN1_PROG.cpp
@@ -1,4 +1,12 @@
-﻿
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
 int main() {
         return 0;
 }
