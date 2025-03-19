8e4e2b525b000bb03643a04a19f38005e3d629e4
Maksim3l <loknarmaksim@gmail.com>
2025-03-19 15:49:00 +0100

    ADD input args
    Added input args for the console app and error handeling

--- ARP_DN1_PROG/ARP_DN1_PROG.cpp
+++ ARP_DN1_PROG/ARP_DN1_PROG.cpp
@@ -7,7 +7,13 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Error handling
+    if (argc != 2) {
+        cerr << endl << "Uporaba: " << argv[0] << endl << "- <vhodna datoteka>" << endl << endl;
+        return 1;
+    }
+
         return 0;
 }
 
